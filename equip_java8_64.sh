9206570bb4dec82216dce02fe177cc7ee7e0d1c1
Brian Lee <blee@orionnetworksolutions.com>
2015-11-04 09:08:35 -0800

    Fixed typos in alternatives Adjusted spacing to better view alternatives

--- equip_java8_64.sh
+++ equip_java8_64.sh
@@ -24,18 +24,18 @@ sudo chown -R root:root /usr/lib/jvm
 sudo chmod -R u=rwX,g=rwX,o=rX /usr/lib/jvm
 sudo chcon -R -u system_u /usr/lib/jvm
 
-sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.8.0_66/bin/java" 1
-sudo update-alternatives --install "/usr/bin/java" "java-1.8.0" "/usr/lib/jvm/jdk1.8.0_66/bin/java" 1
-sudo update-alternatives --install "/usr/bin/java" "java-1.8.0-oracle" "/usr/lib/jvm/jdk1.8.0_66/bin/java" 1
-sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.8.0_66/bin/javac" 1
-sudo update-alternatives --install "/usr/bin/java" "java-1.8.0" "/usr/lib/jvm/jdk1.8.0_66/bin/java" 1
-sudo update-alternatives --install "/usr/bin/javac" "javac-1.8.0-oracle" "/usr/lib/jvm/jdk1.8.0_66/bin/javac" 1
-sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.8.0_66/bin/javaws" 1
-sudo update-alternatives --install "/usr/bin/java" "java-1.8.0" "/usr/lib/jvm/jdk1.8.0_66/bin/java" 1
+sudo update-alternatives --install "/usr/bin/java"   "java"                "/usr/lib/jvm/jdk1.8.0_66/bin/java"   1
+sudo update-alternatives --install "/usr/bin/java"   "java-1.8.0"          "/usr/lib/jvm/jdk1.8.0_66/bin/java"   1
+sudo update-alternatives --install "/usr/bin/java"   "java-1.8.0-oracle"   "/usr/lib/jvm/jdk1.8.0_66/bin/java"   1
+sudo update-alternatives --install "/usr/bin/javac"  "javac"               "/usr/lib/jvm/jdk1.8.0_66/bin/javac"  1
+sudo update-alternatives --install "/usr/bin/java"   "javac-1.8.0"         "/usr/lib/jvm/jdk1.8.0_66/bin/javac"  1
+sudo update-alternatives --install "/usr/bin/javac"  "javac-1.8.0-oracle"  "/usr/lib/jvm/jdk1.8.0_66/bin/javac"  1
+sudo update-alternatives --install "/usr/bin/javaws" "javaws"              "/usr/lib/jvm/jdk1.8.0_66/bin/javaws" 1
+sudo update-alternatives --install "/usr/bin/java"   "javaws-1.8.0"        "/usr/lib/jvm/jdk1.8.0_66/bin/javaws" 1
 sudo update-alternatives --install "/usr/bin/javaws" "javaws-1.8.0-oracle" "/usr/lib/jvm/jdk1.8.0_66/bin/javaws" 1
-sudo update-alternatives --install "/usr/bin/jre" "jre" "/usr/lib/jvm/jdk1.8.0_66/jre" 1
-sudo update-alternatives --install "/usr/bin/java" "java-1.8.0" "/usr/lib/jvm/jdk1.8.0_66/bin/java" 1
-sudo update-alternatives --install "/usr/bin/jre" "jre-1.8.0-oracle" "/usr/lib/jvm/jdk1.8.0_66/jre" 1
+sudo update-alternatives --install "/usr/bin/jre"    "jre"                 "/usr/lib/jvm/jdk1.8.0_66/jre"        1
+sudo update-alternatives --install "/usr/bin/java"   "jre-1.8.0"           "/usr/lib/jvm/jdk1.8.0_66/jre"        1
+sudo update-alternatives --install "/usr/bin/jre"    "jre-1.8.0-oracle"    "/usr/lib/jvm/jdk1.8.0_66/jre"        1
 
 sudo chmod a+x /usr/bin/java
 sudo chmod a+x /usr/bin/javac
