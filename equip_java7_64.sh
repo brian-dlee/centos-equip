f0bf7602e42cba72ad661da62ef2a1b2a3bd807f
Brian Lee <briandl92391@gmail.com>
2015-11-16 12:40:13 -0800

    Create equip_java7_64.sh
    Adding oracle java installer for 7

---
+++ equip_java7_64.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+# Equip 64-bit Java 7u65 on CentOS
+# Author: Brian Lee <briandl92391@gmail.com>, GitHub Username: brian-dlee
+# Licence: MIT
+# Component: java7_64
+# To run, see https://github.com/brian-dlee/centos-equip
+
+if [ -d "/usr/lib/jvm/" ]; then
+	echo "There's already an installation of Java JDK in /usr/lib/jvm."
+    echo "Uninstall the currently installed JDK before running this script."
+	exit 0
+fi
+
+yum install -y curl
+
+curl -L --cookie "oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u65-b17/jdk-7u65-linux-x64.tar.gz -o jdk-7-linux-x64.tar.gz
+tar -xvf jdk-7-linux-x64.tar.gz
+
+mkdir -p /usr/lib/jvm
+mv ./jdk1.7.0_65 /usr/lib/jvm/
+
+chown -R root:root /usr/lib/jvm
+chmod -R u=rwX,g=rwX,o=rX /usr/lib/jvm
+
+if [[ $SELINUX_ENABLED ]]; then
+	chcon -R -u system_u /usr/lib/jvm
+fi
+
+update-alternatives --install "/usr/bin/java"   "java"                "/usr/lib/jvm/jdk1.7.0_65/bin/java"   1
+update-alternatives --install "/usr/bin/java"   "java-1.7.0"          "/usr/lib/jvm/jdk1.7.0_65/bin/java"   1
+update-alternatives --install "/usr/bin/java"   "java-1.7.0-oracle"   "/usr/lib/jvm/jdk1.7.0_65/bin/java"   1
+update-alternatives --install "/usr/bin/javac"  "javac"               "/usr/lib/jvm/jdk1.7.0_65/bin/javac"  1
+update-alternatives --install "/usr/bin/javac"  "javac-1.7.0"         "/usr/lib/jvm/jdk1.7.0_65/bin/javac"  1
+update-alternatives --install "/usr/bin/javac"  "javac-1.7.0-oracle"  "/usr/lib/jvm/jdk1.7.0_65/bin/javac"  1
+update-alternatives --install "/usr/bin/javaws" "javaws"              "/usr/lib/jvm/jdk1.7.0_65/bin/javaws" 1
+update-alternatives --install "/usr/bin/javaws" "javaws-1.7.0"        "/usr/lib/jvm/jdk1.7.0_65/bin/javaws" 1
+update-alternatives --install "/usr/bin/javaws" "javaws-1.7.0-oracle" "/usr/lib/jvm/jdk1.7.0_65/bin/javaws" 1
+update-alternatives --install "/usr/bin/jre"    "jre"                 "/usr/lib/jvm/jdk1.7.0_65/jre"        1
+update-alternatives --install "/usr/bin/jre"    "jre-1.7.0"           "/usr/lib/jvm/jdk1.7.0_65/jre"        1
+update-alternatives --install "/usr/bin/jre"    "jre-1.7.0-oracle"    "/usr/lib/jvm/jdk1.7.0_65/jre"        1
+
+chown -R root:root /usr/lib/jvm/jdk1.7.0_65
+
+chmod a+x /usr/bin/java
+chmod a+x /usr/bin/javac
+chmod a+x /usr/bin/javaws
+
+rm jdk-7-linux-x64.tar.gz
+
+java -version
